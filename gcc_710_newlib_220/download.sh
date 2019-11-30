60440701c725ffc626f09b2e1eff1dd2f89d5559
hayden <819028+mrneo240@users.noreply.github.com>
2019-11-30 16:02:13 -0500

    first commit

---
+++ gcc_710_newlib_220/download.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+
+# These version numbers are all that should ever have to be changed.
+source $PWD/versions.sh
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    case $PARAM in
+        --no-gmp)
+            unset GMP_VER
+            ;;
+        --no-mpfr)
+            unset MPFR_VER
+            ;;
+        --no-mpc)
+            unset MPC_VER
+            ;;
+        --no-deps)
+            unset GMP_VER
+            unset MPFR_VER
+            unset MPC_VER
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Download everything.
+if command -v wget >/dev/null 2>&1; then
+    echo "Downloading binutils-$BINUTILS_VER..."
+    wget –quiet -c ftp://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VER.tar.bz2 || exit 1
+    echo "Downloading GCC $GCC_VER..."
+    wget –quiet -c ftp://ftp.gnu.org/gnu/gcc/gcc-$GCC_VER/gcc-$GCC_VER.tar.gz || exit 1
+    echo "Downloading Newlib $NEWLIB_VER..."
+    wget –quiet -c ftp://sourceware.org/pub/newlib/newlib-$NEWLIB_VER$NEWLIB_EXTRA.tar.gz || exit 1
+
+    if [ -n "$GMP_VER" ]; then
+        echo "Downloading GMP $GMP_VER..."
+        wget –quiet -c ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-$GMP_VER.tar.bz2 || exit 1
+    fi
+
+    if [ -n "$MPFR_VER" ]; then
+        echo "Downloading MPFR $MPFR_VER..."
+        wget –quiet -c ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-$MPFR_VER.tar.bz2 || exit 1
+    fi
+
+    if [ -n "$MPC_VER" ]; then
+        echo "Downloading MPC $MPC_VER..."
+        wget –quiet -c ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-$MPC_VER.tar.gz || exit 1
+    fi
+elif command -v curl >/dev/null 2>&1; then
+    echo "Downloading Binutils $BINUTILS_VER..."
+    curl –silent -C - -O ftp://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VER.tar.bz2 || exit 1
+    echo "Downloading GCC $GCC_VER..."
+    curl –silent -C - -O ftp://ftp.gnu.org/gnu/gcc/gcc-$GCC_VER/gcc-$GCC_VER.tar.bz2 || exit 1
+    echo "Downloading Newlib $NEWLIB_VER..."
+    curl –silent -C - -O ftp://sourceware.org/pub/newlib/newlib-$NEWLIB_VER$NEWLIB_EXTRA.tar.gz || exit 1
+
+    if [ -n "$GMP_VER" ]; then
+        echo "Downloading GMP $GMP_VER..."
+        curl –silent -C - -O ftp://gcc.gnu.org/pub/gcc/infrastructure/gmp-$GMP_VER.tar.bz2 || exit 1
+    fi
+
+    if [ -n "$MPFR_VER" ]; then
+        echo "Downloading MPFR $MPFR_VER..."
+        curl –silent -C - -O ftp://gcc.gnu.org/pub/gcc/infrastructure/mpfr-$MPFR_VER.tar.bz2 || exit 1
+    fi
+
+    if [ -n "$MPC_VER" ]; then
+        echo "Downloading MPC $MPC_VER..."
+        curl –silent -C - -O ftp://gcc.gnu.org/pub/gcc/infrastructure/mpc-$MPC_VER.tar.gz || exit 1
+    fi
+else
+    echo >&2 "You must have either wget or cURL installed to use this script!"
+    exit 1
+fi
---
+++ gcc_710_newlib_220/versions.sh
@@ -0,0 +1,7 @@
+export GCC_VER=7.1.0
+export BINUTILS_VER=2.31.1
+export NEWLIB_VER=2.2.0
+export NEWLIB_EXTRA=.20151023
+export GMP_VER=6.1.0
+export MPFR_VER=3.1.4
+export MPC_VER=1.0.3
